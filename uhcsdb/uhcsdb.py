580c49b5fba76333e0b235719d5f68993da0c12f
Brian DeCost <bdecost@andrew.cmu.edu>
2016-12-14 17:08:43 -0500

    ...

--- uhcsdb/uhcsdb.py
+++ uhcsdb/uhcsdb.py
@@ -87,8 +87,8 @@ def all_entries():
 def favorites():
     db = get_db()
     favs = [73, 357, 137, 156, 223, 290, 354, 359, 363, 372, 379, 394, 404, 422, 450, 452, 472, 696, 785, 830]
-    results = db.query(Micrograph).filter(Micrograph.id.in_(favs))
-    entries = [entry.info() for entry in results]
+    entries = db.query(Micrograph).filter(Micrograph.id.in_(favs))
+    entries = [entry.info() for entry in entries]
     return render_template('all_entries.html', entries=entries)
 
 def paginate(results, page, PER_PAGE):
@@ -106,15 +106,19 @@ def paginate(results, page, PER_PAGE):
   
     return results[start:end], page_data
 
-ENTRIES_PER_PAGE = 24
+PER_PAGE = 24
 @app.route('/')
 @app.route('/index')
 @app.route('/entries/') #, defaults={'page': 1})
 @app.route('/entries/<int:page>')
 def entries(page=1):
     db = get_db()
-    page_results, page_data = paginate(db.query(Micrograph).all(), page, ENTRIES_PER_PAGE)
-    page_entries = [entry.info() for entry in page_results]
+    # all_entries = Micrograph.query.paginate(page, PER_PAGE, False)
+    # all_entries = db.query(Micrograph).paginate(page, PER_PAGE, False)
+    res = db.query(Micrograph).all()
+    page_entries, page_data = paginate(res, page, PER_PAGE)
+    page_entries = [entry.info() for entry in page_entries]
+    count = len(page_entries)
 
     return render_template('show_entries.html', entries=page_entries, pg=page_data)
 
@@ -122,7 +126,8 @@ def entries(page=1):
 def show_entry(entry_id):
     db = get_db()
     entry = db.query(Micrograph).filter(Micrograph.id == entry_id).first()
-    return render_template('show_entry.html', entry=entry.info(), author=entry.user.info())
+    author = entry.user
+    return render_template('show_entry.html', entry=entry.info(), author=author.info())
 
 @app.route('/visual_query/<int:entry_id>')
 def visual_query(entry_id):
@@ -139,22 +144,30 @@ def visual_query(entry_id):
     return render_template('query_results.html', query=query.info(),
                            author=author.info(), results=results)
 
+
+# this should work with flask and ssh-forwarding
+bokeh_process = subprocess.Popen(
+    ['bokeh-3.5', 'serve', '--allow-websocket-origin=localhost:5000',
+     '--log-level=debug', 'visualize.py'], stdout=subprocess.PIPE)
+
+@atexit.register
+def kill_server():
+    bokeh_process.kill()
+
+
 @app.route('/visualize')
 def bokeh_plot():
     session=pull_session(app_path='/visualize')
     bokeh_script=autoload_server(None,app_path="/visualize",session_id=session.id)
+    # session=pull_session(app_path='/sliders')
+    # bokeh_script=autoload_server(model=None, app_path='/sliders', session_id=session.id)
+    # return render_template_string(app_html, bokeh_script=bokeh_script)
     return render_template('visualize.html', bokeh_script=bokeh_script)
 
 if __name__ == '__main__':
-    # this should work with flask and ssh-forwarding
-    bokeh_process = subprocess.Popen(
-        ['bokeh-3.5', 'serve', '--allow-websocket-origin=localhost:5000',
-         '--log-level=debug', 'visualize.py'], stdout=subprocess.PIPE)
-
-    @atexit.register
-    def kill_server():
-        bokeh_process.kill()
 
     app.config.from_object('config')
     with app.app_context():
+        # db.metadata.create_all(bind=db.engine)
         app.run(debug=False)
+        # app.run(host='0.0.0.0', debug=False)
